f5c5aafe2158fca22daff197e97843a387ac09ea
Raven Medicine <123703127+raven-medicine@users.noreply.github.com>
2023-01-29 11:54:55 -0500

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,8 +1,233 @@
-echo 'Checking Current User'
-whoami
-echo 'Check Network Status'
-ip a
-echo 'Check Kernel Version'
-uname -an
-echo 'Check /etc/passwd'
-cat /etc/passwd
+#!/bin/bash
+#A script to enumerate local information from a Linux host
+version="version 0.982"
+#@rebootuser
+
+#help function
+usage () 
+{ 
+echo -e "\n\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;31m#\e[00m" "\e[00;33mLocal Linux Enumeration & Privilege Escalation Script\e[00m" "\e[00;31m#\e[00m"
+echo -e "\e[00;31m#########################################################\e[00m"
+echo -e "\e[00;33m# www.rebootuser.com | @rebootuser \e[00m"
+echo -e "\e[00;33m# $version\e[00m\n"
+echo -e "\e[00;33m# Example: ./LinEnum.sh -k keyword -r report -e /tmp/ -t \e[00m\n"
+
+		echo "OPTIONS:"
+		echo "-k	Enter keyword"
+		echo "-e	Enter export location"
+		echo "-s 	Supply user password for sudo checks (INSECURE)"
+		echo "-t	Include thorough (lengthy) tests"
+		echo "-r	Enter report name" 
+		echo "-h	Displays this help text"
+		echo -e "\n"
+		echo "Running with no options = limited scans/no output file"
+		
+echo -e "\e[00;31m#########################################################\e[00m"		
+}
+header()
+{
+echo -e "\n\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;31m#\e[00m" "\e[00;33mLocal Linux Enumeration & Privilege Escalation Script\e[00m" "\e[00;31m#\e[00m" 
+echo -e "\e[00;31m#########################################################\e[00m" 
+echo -e "\e[00;33m# www.rebootuser.com\e[00m" 
+echo -e "\e[00;33m# $version\e[00m\n" 
+
+}
+
+debug_info()
+{
+echo "[-] Debug Info" 
+
+if [ "$keyword" ]; then 
+	echo "[+] Searching for the keyword $keyword in conf, php, ini and log files" 
+fi
+
+if [ "$report" ]; then 
+	echo "[+] Report name = $report" 
+fi
+
+if [ "$export" ]; then 
+	echo "[+] Export location = $export" 
+fi
+
+if [ "$thorough" ]; then 
+	echo "[+] Thorough tests = Enabled" 
+else 
+	echo -e "\e[00;33m[+] Thorough tests = Disabled\e[00m" 
+fi
+
+sleep 2
+
+if [ "$export" ]; then
+  mkdir $export 2>/dev/null
+  format=$export/LinEnum-export-`date +"%d-%m-%y"`
+  mkdir $format 2>/dev/null
+fi
+
+if [ "$sudopass" ]; then 
+  echo -e "\e[00;35m[+] Please enter password - INSECURE - really only for CTF use!\e[00m"
+  read -s userpassword
+  echo 
+fi
+
+who=`whoami` 2>/dev/null 
+echo -e "\n" 
+
+echo -e "\e[00;33mScan started at:"; date 
+echo -e "\e[00m\n" 
+}
+
+# useful binaries (thanks to https://gtfobins.github.io/)
+binarylist='aria2c\|arp\|ash\|awk\|base64\|bash\|busybox\|cat\|chmod\|chown\|cp\|csh\|curl\|cut\|dash\|date\|dd\|diff\|dmsetup\|docker\|ed\|emacs\|env\|expand\|expect\|file\|find\|flock\|fmt\|fold\|ftp\|gawk\|gdb\|gimp\|git\|grep\|head\|ht\|iftop\|ionice\|ip$\|irb\|jjs\|jq\|jrunscript\|ksh\|ld.so\|ldconfig\|less\|logsave\|lua\|make\|man\|mawk\|more\|mv\|mysql\|nano\|nawk\|nc\|netcat\|nice\|nl\|nmap\|node\|od\|openssl\|perl\|pg\|php\|pic\|pico\|python\|readelf\|rlwrap\|rpm\|rpmquery\|rsync\|ruby\|run-parts\|rvim\|scp\|script\|sed\|setarch\|sftp\|sh\|shuf\|socat\|sort\|sqlite3\|ssh$\|start-stop-daemon\|stdbuf\|strace\|systemctl\|tail\|tar\|taskset\|tclsh\|tee\|telnet\|tftp\|time\|timeout\|ul\|unexpand\|uniq\|unshare\|vi\|vim\|watch\|wget\|wish\|xargs\|xxd\|zip\|zsh'
+
+system_info()
+{
+echo -e "\e[00;33m### SYSTEM ##############################################\e[00m" 
+
+#basic kernel info
+unameinfo=`uname -a 2>/dev/null`
+if [ "$unameinfo" ]; then
+  echo -e "\e[00;31m[-] Kernel information:\e[00m\n$unameinfo" 
+  echo -e "\n" 
+fi
+
+procver=`cat /proc/version 2>/dev/null`
+if [ "$procver" ]; then
+  echo -e "\e[00;31m[-] Kernel information (continued):\e[00m\n$procver" 
+  echo -e "\n" 
+fi
+
+#search all *-release files for version info
+release=`cat /etc/*-release 2>/dev/null`
+if [ "$release" ]; then
+  echo -e "\e[00;31m[-] Specific release information:\e[00m\n$release" 
+  echo -e "\n" 
+fi
+
+#target hostname info
+hostnamed=`hostname 2>/dev/null`
+if [ "$hostnamed" ]; then
+  echo -e "\e[00;31m[-] Hostname:\e[00m\n$hostnamed" 
+  echo -e "\n" 
+fi
+}
+
+user_info()
+{
+echo -e "\e[00;33m### USER/GROUP ##########################################\e[00m" 
+
+#current user details
+currusr=`id 2>/dev/null`
+if [ "$currusr" ]; then
+  echo -e "\e[00;31m[-] Current user/group info:\e[00m\n$currusr" 
+  echo -e "\n"
+fi
+
+#last logged on user information
+lastlogedonusrs=`lastlog 2>/dev/null |grep -v "Never" 2>/dev/null`
+if [ "$lastlogedonusrs" ]; then
+  echo -e "\e[00;31m[-] Users that have previously logged onto the system:\e[00m\n$lastlogedonusrs" 
+  echo -e "\n" 
+fi
+
+#who else is logged on
+loggedonusrs=`w 2>/dev/null`
+if [ "$loggedonusrs" ]; then
+  echo -e "\e[00;31m[-] Who else is logged on:\e[00m\n$loggedonusrs" 
+  echo -e "\n"
+fi
+
+#lists all id's and respective group(s)
+grpinfo=`for i in $(cut -d":" -f1 /etc/passwd 2>/dev/null);do id $i;done 2>/dev/null`
+if [ "$grpinfo" ]; then
+  echo -e "\e[00;31m[-] Group memberships:\e[00m\n$grpinfo"
+  echo -e "\n"
+fi
+
+#added by phackt - look for adm group (thanks patrick)
+adm_users=$(echo -e "$grpinfo" | grep "(adm)")
+if [[ ! -z $adm_users ]];
+  then
+    echo -e "\e[00;31m[-] It looks like we have some admin users:\e[00m\n$adm_users"
+    echo -e "\n"
+fi
+
+#checks to see if any hashes are stored in /etc/passwd (depreciated  *nix storage method)
+hashesinpasswd=`grep -v '^[^:]*:[x]' /etc/passwd 2>/dev/null`
+if [ "$hashesinpasswd" ]; then
+  echo -e "\e[00;33m[+] It looks like we have password hashes in /etc/passwd!\e[00m\n$hashesinpasswd" 
+  echo -e "\n"
+fi
+
+#contents of /etc/passwd
+readpasswd=`cat /etc/passwd 2>/dev/null`
+if [ "$readpasswd" ]; then
+  echo -e "\e[00;31m[-] Contents of /etc/passwd:\e[00m\n$readpasswd" 
+  echo -e "\n"
+fi
+
+if [ "$export" ] && [ "$readpasswd" ]; then
+  mkdir $format/etc-export/ 2>/dev/null
+  cp /etc/passwd $format/etc-export/passwd 2>/dev/null
+fi
+
+#checks to see if the shadow file can be read
+readshadow=`cat /etc/shadow 2>/dev/null`
+if [ "$readshadow" ]; then
+  echo -e "\e[00;33m[+] We can read the shadow file!\e[00m\n$readshadow" 
+  echo -e "\n"
+fi
+
+if [ "$export" ] && [ "$readshadow" ]; then
+  mkdir $format/etc-export/ 2>/dev/null
+  cp /etc/shadow $format/etc-export/shadow 2>/dev/null
+fi
+
+#checks to see if /etc/master.passwd can be read - BSD 'shadow' variant
+readmasterpasswd=`cat /etc/master.passwd 2>/dev/null`
+if [ "$readmasterpasswd" ]; then
+  echo -e "\e[00;33m[+] We can read the master.passwd file!\e[00m\n$readmasterpasswd" 
+  echo -e "\n"
+fi
+
+if [ "$export" ] && [ "$readmasterpasswd" ]; then
+  mkdir $format/etc-export/ 2>/dev/null
+  cp /etc/master.passwd $format/etc-export/master.passwd 2>/dev/null
+fi
+
+#all root accounts (uid 0)
+superman=`grep -v -E "^#" /etc/passwd 2>/dev/null| awk -F: '$3 == 0 { print $1}' 2>/dev/null`
+if [ "$superman" ]; then
+  echo -e "\e[00;31m[-] Super user account(s):\e[00m\n$superman"
+  echo -e "\n"
+fi
+
+#pull out vital sudoers info
+sudoers=`grep -v -e '^$' /etc/sudoers 2>/dev/null |grep -v "#" 2>/dev/null`
+if [ "$sudoers" ]; then
+  echo -e "\e[00;31m[-] Sudoers configuration (condensed):\e[00m$sudoers"
+  echo -e "\n"
+fi
+
+if [ "$export" ] && [ "$sudoers" ]; then
+  mkdir $format/etc-export/ 2>/dev/null
+  cp /etc/sudoers $format/etc-export/sudoers 2>/dev/null
+fi
+
+#can we sudo without supplying a password
+sudoperms=`echo '' | sudo -S -l -k 2>/dev/null`
+if [ "$sudoperms" ]; then
+  echo -e "\e[00;33m[+] We can sudo without supplying a password!\e[00m\n$sudoperms" 
+  echo -e "\n"
+fi
+
+#check sudo perms - authenticated
+if [ "$sudopass" ]; then
+    if [ "$sudoperms" ]; then
+      :
+    else
+      sudoauth=`echo $userpassword | sudo -S -l -k 2>/dev/null`
+      if [ "$sudoauth" ]; then
+        echo -e "\e[00;33m[+] We can sudo when supplying a password!\e[00m\n$sudoauth" 
+        echo -e "\n"
